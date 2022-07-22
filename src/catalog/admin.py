493c74cfffa6879b6169ab8291222518ecbee5e9
greatlaki <greatlaki911@gmail.com>
2022-07-22 14:18:45 +0300

    managing displayed and nested fields

--- src/catalog/admin.py
+++ src/catalog/admin.py
@@ -6,6 +6,7 @@ from .models import *
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 
 # Register the admin class with the associated model
