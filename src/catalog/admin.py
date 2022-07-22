e8ad86c1bba3053d2ee72349b40b2ffd3d436290
greatlaki <greatlaki911@gmail.com>
2022-07-22 14:22:44 +0300

    built-in editing of related records

--- src/catalog/admin.py
+++ src/catalog/admin.py
@@ -13,10 +13,15 @@ class AuthorAdmin(admin.ModelAdmin):
 admin.site.register(Author, AuthorAdmin)
 
 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
 
 # Register the Admin classes for BookInstance using the decorator
