a54bb11a1a5bf7070671890d5d07a205f49b6da3
greatlaki <greatlaki911@gmail.com>
2022-07-22 14:12:32 +0300

    configuring the display of lists

--- src/catalog/admin.py
+++ src/catalog/admin.py
@@ -5,7 +5,7 @@ from .models import *
 
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
 
 # Register the admin class with the associated model
@@ -15,7 +15,7 @@ admin.site.register(Author, AuthorAdmin)
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'display_genre')
 
 
 # Register the Admin classes for BookInstance using the decorator
