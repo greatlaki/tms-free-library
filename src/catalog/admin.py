9b3137bcab9817e2b35d5bedfd358eda4981c6eb
greatlaki <greatlaki911@gmail.com>
2022-07-22 14:00:30 +0300

    register model via class

--- src/catalog/admin.py
+++ src/catalog/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 
 from .models import *
 
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+
 admin.site.register(Book)
-admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(BookInstance)
