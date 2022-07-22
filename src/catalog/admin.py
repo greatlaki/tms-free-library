beeca2eece246364166bc30fb3709549310261c8
greatlaki <greatlaki911@gmail.com>
2022-07-22 14:26:24 +0300

    update

--- src/catalog/admin.py
+++ src/catalog/admin.py
@@ -2,31 +2,35 @@ from django.contrib import admin
 
 from .models import *
 
+admin.site.register(Genre)
+admin.site.register(Language)
 
-# Define the admin class
+
+class BooksInline(admin.TabularInline):
+    """Defines format of inline book insertion (used in AuthorAdmin)"""
+    model = Book
+
+
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-
-
-# Register the admin class with the associated model
-admin.site.register(Author, AuthorAdmin)
+    inlines = [BooksInline]
 
 
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
 
 
-# Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
     inlines = [BooksInstanceInline]
 
 
-# Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
@@ -38,7 +42,3 @@ class BookInstanceAdmin(admin.ModelAdmin):
         }),
     )
 
-
-admin.site.register(Genre)
-admin.site.register(BookInstance)
-admin.site.register(Language)
