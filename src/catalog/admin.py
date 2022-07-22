63b33e7e454e222e643c713c52b8081ca1c40963
greatlaki <greatlaki911@gmail.com>
2022-07-22 14:19:59 +0300

    division into sections/highlighting a detailed view

--- src/catalog/admin.py
+++ src/catalog/admin.py
@@ -24,6 +24,15 @@ class BookAdmin(admin.ModelAdmin):
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
 
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
 
 admin.site.register(Genre)
 admin.site.register(BookInstance)
