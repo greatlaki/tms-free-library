437fb01a5dcb2688c1972287674290fc81bc43f2
greatlaki <greatlaki911@gmail.com>
2022-07-23 17:03:22 +0300

    function of view of homepage was modified

--- src/catalog/views.py
+++ src/catalog/views.py
@@ -4,25 +4,25 @@ from .models import *
 
 
 def index(request):
-    """
-    Функция отображения для домашней страницы сайта.
-    """
-    # Генерация "количеств" некоторых главных объектов
+    """View function for home page of site."""
+    # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-    # Доступные книги (статус = 'a')
+    # Available copies of books
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_authors = Author.objects.count()  # The 'all()' is implied by default.
 
-    # Отрисовка HTML-шаблона index.html с данными внутри
-    # переменной контекста context
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits+1
+
+    # Render the HTML template index.html with the data in the context variable.
     return render(
         request,
-        'catalog/index.html',
-        context={
-            'num_books': num_books,
-            'num_instances': num_instances,
-            'num_instances_available': num_instances_available,
-            'num_authors': num_authors,
-        },
+        'index.html',
+        context={'num_books': num_books,
+                 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available,
+                 'num_authors': num_authors,
+                 'num_visits': num_visits},
     )
