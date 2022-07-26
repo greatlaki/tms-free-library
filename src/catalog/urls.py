36730a4c69aa96958a7fd1ee80dee1f23a69ed36
greatlaki <greatlaki911@gmail.com>
2022-07-26 17:06:27 +0300

    authentication urls were added

--- src/catalog/urls.py
+++ src/catalog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
@@ -8,4 +8,10 @@ urlpatterns = [
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+]
+
+
+# Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
