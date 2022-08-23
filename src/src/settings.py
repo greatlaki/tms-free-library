783984d3f724a0704d05fb99bd0a09bbb6c5aed8
greatlaki <greatlaki911@gmail.com>
2022-08-23 15:56:16 +0300

    update

--- src/src/settings.py
+++ src/src/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
